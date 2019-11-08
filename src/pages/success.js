eccae2a0ec765a333d9dbe3fc126c42d59765be4
dan214 <irungu214@gmail.com>
2019-11-08 02:35:29 -0800

    add success page

---
+++ src/pages/success.js
@@ -0,0 +1,16 @@
+
+
+import React from "react";
+import Layout from "../components/layout";
+
+const Success = () => {
+
+    return (
+        <Layout>
+            <p>Thank you for your submission</p>
+        
+        </Layout>
+    );
+};
+
+export default Success;
