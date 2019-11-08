54f78431dd0cce2d3e84c606df586c30bab43ad1
dan214 <irungu214@gmail.com>
2019-11-08 04:02:35 -0800

    check in form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,7 +10,8 @@ const Contact = () => {
                 <div className="news-item contact-div contact-us">
                     <div className="">
                 <h2>{"We would love to hear from you!"}</h2>
-                    <form name="contact" action="/success" method="POST" data-netlify="true">
+                    <form name="contact" netlify-honeypot="bot-field" action="/success" method="POST" data-netlify="true">
+                    <input type="hidden" name="bot-field" />
                         <div class="form-group row">
 
                             <label className="col-lg-2">Name: </label>
