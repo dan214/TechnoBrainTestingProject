7ca608ef2d02e83d4e7192dc6fa6d3d9854c07e0
dan214 <irungu214@gmail.com>
2019-11-08 02:30:43 -0800

    add contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,11 +3,43 @@ import Layout from "../components/layout";
 
 const Contact = () => {
 
-  return (
-    <Layout>
-        <p>Contact U page. Yeeeeh!</p>      
-    </Layout>
-  );
+    return (
+        <Layout>
+        
+            <div className="container">
+                <div className="news-item contact-div contact-us">
+                    <div className="">
+                <h2>{"We would love to hear from you!"}</h2>
+                    <form name="contact" method="POST" data-netlify="true">
+                        <div class="form-group row">
+
+                            <label className="col-lg-2">Name: </label>
+                            <input type="text" className="form-control col-lg-6" name="name" />
+
+                        </div>
+                        <div class="form-group row">
+
+                            <label className="col-lg-2">Email: </label>
+                            <input className="form-control col-lg-6" type="email" name="email" />
+
+                        </div>
+
+                        <div class="form-group row">
+
+                            <label className="col-lg-2">Message: </label>
+                            <textarea className="form-control col-lg-6" name="message"></textarea>
+
+                        </div>
+
+                        <button className="btn" type="submit">Send</button>
+
+                    </form>
+                    </div>
+                </div>
+            </div>
+
+        </Layout>
+    );
 };
 
 export default Contact;
